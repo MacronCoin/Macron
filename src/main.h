f9b85080a084066f93537882bbfd0f4a9bf1c0eb
LePen <Martine.LePen@outlook.com>
2017-02-19 09:36:19 +0100

    MacronCoin

--- src/main.h
+++ src/main.h
@@ -30,7 +30,7 @@ class CInv;
 class CRequestTracker;
 class CNode;
 
-static const int LAST_POW_BLOCK = 2000;
+static const int LAST_POW_BLOCK = 5000;
 
 static const unsigned int MAX_BLOCK_SIZE = 40000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
